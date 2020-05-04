613e6a0712bdfd286132f637b0307ed71c49da21
Theodore Quinn <tcquinn@tcquinn.org>
2020-05-04 09:00:38 -0500

    Add Layer class

--- simple_neural_net/__init__.py
+++ simple_neural_net/__init__.py
@@ -1,1 +1,2 @@
 from simple_neural_net.core import *
+from simple_neural_net.layers import *
---
+++ simple_neural_net/layers.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Layer:
+    def __init__(
+        num_inputs,
+        num_outputs,
+        activation_function,
+        weight_initialization_multiplier = 0.01,
+        bias_initialization_multiplier = 0.0
+    ):
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.activation_function = activation_function
+        self.weight_initialization_multiplier = weight_initialization_multiplier
+        self.bias_initialization_multiplier = bias_initialization_multiplier
+        self.weights = np.multiply(
+            np.random.randn(self.num_outputs, self.num_inputs),
+            self.weight_initialization_multiplier
+        )
+        self.biases = np.multiply(
+            np.random.randn(self.num_outputs, 1),
+            self.bias_initialization_multiplier
+        )
+
+    def forward_propagate(inputs):
+        # inputs: (num_inputs, num_examples)
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        self.inputs = inputs # (num_inputs, num_examples)
+        self.Z = np.dot(self.weights, self.inputs) + self.biases # (n_outputs, num_examples)
+        self.outputs = self.activation_function.activation(self.Z) # (num_outputs, num_examples)
+        return outputs
+
+    def backward_propagate(d_cost_d_outputs):
+        # d_cost_d_outputs: (num_outputs, num_examples)
+        # d_outputs_d_Z: (num_outputs, num_examples)
+        # d_cost_d_Z: (num_outputs, num_examples)
+        # d_Z_d_weights: (?)
+        # d_cost_d_weights (num_outputs, num_inputs)
+        self.d_cost_d_outputs = d_cost_d_outputs
+        self.d_cost_d_Z = self.d_cost_d_outputs * self.activation_function.d_activation_d_Z(self.Z, self.outputs) # (num_outputs, num_examples)
+        self.d_cost_d_weights = np.dot(self.d_cost_d_Z, self.inputs.T) # (n_outputs, n_inputs)
+        self.d_cost_d_biases = np.sum(d_cost_d_Z, axis=1, keepdims=True) # (n_outputs, 1)
+        self.d_cost_d_inputs = np.dot(self.weights.T, self.d_cost_d_Z) # (n_inputs, num_training_examples)
+        return d_cost_d_inputs
