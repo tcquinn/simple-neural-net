c9ab17f7ae9cd21417564b8ebbcac5fc7909548d
Theodore Quinn <tcquinn@tcquinn.org>
2020-05-04 14:45:14 -0500

    Add NeuralNet class

--- simple_neural_net/core.py
+++ simple_neural_net/core.py
@@ -1,1 +1,26 @@
-# START CODE HERE
+
+class NeuralNet:
+    def __init__(
+        layers,
+        cost,
+    ):
+        self.layers = layers
+        self.cost = cost
+        self.num_layers = len(layers)
+
+    def train(
+        num_iterations,
+        learning_rate,
+        ground_truth_inputs,
+        ground_truth_outputs
+    ):
+        for iteration_index in num_layers:
+            inputs = ground_truth_inputs
+            for layer_index in range(num_layers):
+                outputs = layers[layer_index].forward_propagate(inputs)
+                inputs = outputs
+            self.cost = cost.cost(outputs, ground_truth_outputs)
+            d_cost_d_outputs = cost.d_cost_d_outputs(outputs, ground_truth)
+            for layer_index in reversed(range(num_layers)):
+                d_cost_d_inputs = layers[layer_index].backward_propagate(d_cost_d_outputs)
+                d_cost_d_outputs = d_cost_d_inputs
